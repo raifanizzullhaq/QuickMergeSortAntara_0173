ec1a7b1bdcd8cd4631051dd26773f887d5a04220
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 15:49:01 +0700

    memasukkan library dan menambahkan int array

---
+++ QuickSort.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel array untuk menampung data
+int cmp_count = 0; // Deklarasi variabel untuk jumlah perbandingan
+int mov_count = 0; // Deklarasi variabel untuk jumlah pergeseran data
+int n; // Deklarasi variabel n untuk panjang elemen array
