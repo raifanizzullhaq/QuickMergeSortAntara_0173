8f7c8758b91c0af6d50fe7902e5fd9fc12f3fad0
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 15:14:36 +0700

    isi mergesort

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -5,3 +5,19 @@ int arr {20}; //inisialisasi variabel array untuk menampung nilai
 int cmp_count = 0; //inisialisasi variabel untuk jumlah perbandingan
 int mov_count = 0; //inisialisasi variabel untuk jumlah pergerakan data
 int n; //inisialisasi variabel n untuk panjang element array
+
+void input() //membuat prosedur input
+{
+    while (true) //looping untuk meminta input panjang element array
+    {
+        cout << "masukkan panjang element array : ";
+        cin >> n;
+        
+        if (n <= 20) //jika jumlah element kurang dari atau sama dengan 20 looping selesai
+        {
+            break;
+        }
+        else //jika jumlah element lebih dari 20 permintaan input akan di ulang
+        {
+            cout << "\nmaksimal panjang array adalah 20";
+        }
