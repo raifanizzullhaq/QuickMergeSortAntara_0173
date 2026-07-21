54411dffc506930bf3e82a5b46af5f6e240c6a2d
raifanizzullhaq <raifanizzulhaq3>
2026-07-21 14:48:24 +0700

    inisialisasi variabel

---
+++ Mergesort.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int arr {20}; //inisialisasi variabel array untuk menampung nilai
+int cmp_count = 0; //inisialisasi variabel untuk jumlah perbandingan
+int mov_count = 0; //inisialisasi variabel untuk jumlah pergerakan data
+int n; //inisialisasi variabel n untuk panjang element array
